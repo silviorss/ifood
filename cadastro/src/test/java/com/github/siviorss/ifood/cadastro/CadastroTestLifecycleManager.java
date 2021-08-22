d081f7b8a18e45b39ed9dc5c046c6a4d6f73464f
Silvio Romero <romero_394@hotmail.com>
2021-08-22 00:57:48 -0300

    Adicionando TestContainers, Database Rider e Approval Tests

---
+++ cadastro/src/test/java/com/github/siviorss/ifood/cadastro/CadastroTestLifecycleManager.java
@@ -0,0 +1,34 @@
+package com.github.siviorss.ifood.cadastro;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class CadastroTestLifecycleManager implements QuarkusTestResourceLifecycleManager {
+
+	public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:12.2");
+	
+	@Override
+	public Map<String, String> start() {
+		POSTGRES.start();
+		
+		Map<String, String> properties = new HashMap<String, String>();
+		
+		properties.put("quarkus.datasource.jdbc.url", POSTGRES.getJdbcUrl());
+		properties.put("quarkus.datasource.username", POSTGRES.getUsername());
+		properties.put("quarkus.datasource.password", POSTGRES.getPassword());
+		
+		return properties;
+	}
+
+	@Override
+	public void stop() {
+		if(POSTGRES != null && POSTGRES.isRunning()) {
+			POSTGRES.stop();
+		}
+	}
+
+}
---
+++ cadastro/src/test/java/com/github/siviorss/ifood/cadastro/RestauranteResourceTest.java
@@ -0,0 +1,32 @@
+package com.github.siviorss.ifood.cadastro;
+
+import static io.restassured.RestAssured.given;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+import com.github.database.rider.cdi.api.DBRider;
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.configuration.Orthography;
+import com.github.database.rider.core.api.dataset.DataSet;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+
+@DBRider
+@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
+@QuarkusTest
+@QuarkusTestResource(CadastroTestLifecycleManager.class)
+public class RestauranteResourceTest {
+
+	@Test
+	@DataSet("restaurantes-cenario-1.yml")
+	public void testGetAll() {
+		String resultado = given()
+				.when().get("/restaurantes")
+				.then()
+				.statusCode(200)
+				.extract().asString();
+		Approvals.verifyJson(resultado);
+	}
+}
