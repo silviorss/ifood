351ed0d8b52aabf3914978e4bebce99bb86d98e9
Silvio Romero <romero_394@hotmail.com>
2021-08-22 01:26:49 -0300

    Adicionando dependencia do MapStruct para utilizar nos dto's

--- cadastro/src/main/java/com/github/siviorss/ifood/cadastro/entity/Restaurante.java
+++ cadastro/src/main/java/com/github/siviorss/ifood/cadastro/entity/Restaurante.java
@@ -2,11 +2,13 @@ package com.github.siviorss.ifood.cadastro.entity;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -28,7 +30,7 @@ public class Restaurante extends PanacheEntityBase {
 	
 	public String nome;
 	
-	@ManyToOne
+	@OneToOne(cascade = CascadeType.ALL)
 	public Localizacao localizacao; 
 	
 	@CreationTimestamp
