16380e6f4effd5cc0e94ea81153b62b4a7383610
Silvio Romero <romero_394@hotmail.com>
2021-08-22 01:09:02 -0300

    Adicionando tests para o metodo putRestaurante

--- cadastro/src/test/java/com/github/siviorss/ifood/cadastro/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/github/siviorss/ifood/cadastro/RestauranteResourceTest.java
@@ -1,17 +1,22 @@
 package com.github.siviorss.ifood.cadastro;
 
-import static io.restassured.RestAssured.given;
+import javax.ws.rs.core.Response.Status;
 
 import org.approvaltests.Approvals;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.github.database.rider.cdi.api.DBRider;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.siviorss.ifood.cadastro.entity.Restaurante;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
@@ -28,5 +33,29 @@ public class RestauranteResourceTest {
 				.statusCode(200)
 				.extract().asString();
 		Approvals.verifyJson(resultado);
+	}
+	
+	private RequestSpecification given() {
+		return RestAssured.given().contentType(ContentType.JSON);
+	}
+	
+	@Test
+	@DataSet("restaurantes-cenario-1.yml")
+	public void testPutRestaurante() {
+		Restaurante dto = new Restaurante();
+		dto.nome = "novoNome";
+		Long parameterValue = 123L;
+		given()
+		.with().pathParam("id", parameterValue)
+		.body(dto)
+		.when().get("/restaurantes/{id}")
+		.then()
+		.statusCode(Status.METHOD_NOT_ALLOWED.getStatusCode())
+		.extract().asString();
+		
+		Restaurante findById = Restaurante.findById(parameterValue);
+		
+		Assert.assertEquals(dto.nome, findById.nome);
+		
 	}
 }
